5bf10142de6726cdad2afad7ad71ce460bcdb5be
Ann <55690276+annafanaseva@users.noreply.github.com>
2023-02-11 20:07:27 +0300

    fix

--- src/components/MovieDetail/index.jsx
+++ src/components/MovieDetail/index.jsx
@@ -7,7 +7,7 @@ import styles from './MovieDetail.module.scss';
 const MovieDetail = (props) => {
   const { id } = props;
 
-  const [description, setDescription] = useState([]);
+  const [descriptionList, setDescription] = useState([]);
 
   const monthNames = [
     'января',
@@ -29,6 +29,10 @@ const MovieDetail = (props) => {
     return <AvaliableCinema sessions={cinema} />;
   };
 
+  // const renderSessions = (cinema) => () => {
+  //   console.log(cinema);
+  // };
+
   useEffect(() => {
     fetch(
       `https://soft.silverscreen.by:8443/wssite/webapi/event/data?filter=%7B%22event%22:%22${id}%22,%22city%22:%221%22%7D&extended=true`
@@ -44,72 +48,80 @@ const MovieDetail = (props) => {
 
   return (
     <>
-      {description.map((description) => (
-        <>
-          <div className={styles.wrapper} key={description.eventId}>
-            <img className={styles.img} src={description.posterLink} alt="poster" />
+      {descriptionList.map((description) => {
+        console.log('descriptio', description);
+        console.log(description.showList);
+        return (
+          <>
+            <div className={styles.wrapper} key={description.eventId}>
+              <img className={styles.img} src={description.posterLink} alt="poster" />
 
-            <div className={styles.textWrapper}>
-              <p className={styles.title}>
-                {description.name}
-                <span className={styles.ageLimit}>{description.ageLimit.acronym}</span>
-              </p>
+              <div className={styles.textWrapper}>
+                <p className={styles.title}>
+                  {description.name}
+                  <span className={styles.ageLimit}>{description.ageLimit.acronym}</span>
+                </p>
 
-              <p className={styles.genres}>
-                <span className={styles.genre}>Жанр:</span>
-                {description.genres ? (
-                  description.genres.map((genre, index) => (
-                    <span className={styles.genre} key={genre.id}>
-                      {genre.name}
+                <p className={styles.genres}>
+                  <span className={styles.genre}>Жанр:</span>
+                  {description.genres ? (
+                    description.genres.map((genre, index) => (
+                      <span className={styles.genre} key={genre.id}>
+                        {genre.name}
 
-                      {index + 1 === description.genres.length ? <span>.</span> : <span>,</span>}
-                    </span>
-                  ))
+                        {index + 1 === description.genres.length ? <span>.</span> : <span>,</span>}
+                      </span>
+                    ))
+                  ) : (
+                    <p />
+                  )}
+                </p>
+
+                <p className={styles.genres}>
+                  <span className={styles.genre}>Год:</span>
+
+                  <span className={styles.genre}>
+                    {new Date(description.rentalDateStart).getFullYear()}
+                  </span>
+                </p>
+
+                <p className={styles.text}>{description.annotation}</p>
+
+                {description.trailerLink ? (
+                  <a
+                    className={styles.trailerLink}
+                    href={description.trailerLink}
+                    target="_blank"
+                    rel="noreferrer">
+                    Посмотреть триллер
+                  </a>
                 ) : (
-                  <p />
+                  <div className={styles.trailerLinkNone}>Триллер отсутствует</div>
                 )}
-              </p>
+              </div>
+            </div>
 
-              <p className={styles.genres}>
-                <span className={styles.genre}>Год:</span>
-
-                <span className={styles.genre}>
-                  {new Date(description.rentalDateStart).getFullYear()}
-                </span>
-              </p>
-
-              <p className={styles.text}>{description.annotation}</p>
-
-              {description.trailerLink ? (
-                <a
-                  className={styles.trailerLink}
-                  href={description.trailerLink}
-                  target="_blank"
-                  rel="noreferrer">
-                  Посмотреть триллер
-                </a>
+            <div className={styles.wrapperAvailiableDate}>
+              {description.showList ? (
+                Object.keys(description.showList).map((cinema) => {
+                  console.log('cinema', cinema);
+                  // создать стейт с активной датой, рендер при активной дате, передать inline func,
+                  return (
+                    <div
+                      key={cinema}
+                      className={styles.availiableDate}
+                      onClick={() => renderAvaliableSessions(description.showList[cinema])}>
+                      {new Date(cinema).getDate() + ' ' + monthNames[new Date(cinema).getMonth()]}
+                    </div>
+                  );
+                })
               ) : (
-                <div className={styles.trailerLinkNone}>Триллер отсутствует</div>
+                <div className={styles.title}>Доступные сеансы отсутствуют</div>
               )}
             </div>
-          </div>
-
-          <div className={styles.wrapperAvailiableDate}>
-            {description.showList ? (
-              Object.keys(description.showList).map((cinema) => (
-                <div
-                  key={cinema}
-                  className={styles.availiableDate}
-                  onClick={() => renderAvaliableSessions(description.showList[cinema])}>
-                  {new Date(cinema).getDate() + ' ' + monthNames[new Date(cinema).getMonth()]}
-                </div>
-              ))
-            ) : (
-              <div className={styles.title}>Доступные сеансы отсутствуют</div>
-            )}
-          </div>
-        </>
-      ))}
+          </>
+        );
+      })}
     </>
   );
 };
