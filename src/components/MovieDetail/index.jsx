c0a5c6467b5a8a6ab7790ffb262b859ff4da15d5
Afanaseva Ann <ann.afanaseva777@gmail.com>
2023-02-21 14:40:00 +0200

    fix

--- src/components/MovieDetail/index.jsx
+++ src/components/MovieDetail/index.jsx
@@ -10,11 +10,12 @@ const MovieDetail = (props) => {
 
   const [descriptionList, setDescription] = useState([]);
 
-  //const [count, setCount] = useState();
+  const [sessionDate, setSessionDate] = useState();
 
   const renderAvaliableSessions = (cinema) => {
     console.log(cinema);
-    return <AvaliableCinema sessions={cinema} />;
+    setSessionDate(cinema);
+    <AvaliableCinema sessions={sessionDate} />;
   };
 
   // const renderSessions = (cinema) => () => {
@@ -29,7 +30,6 @@ const MovieDetail = (props) => {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
         setDescription(data);
       });
   }, [id]);
@@ -37,68 +37,69 @@ const MovieDetail = (props) => {
   return (
     <>
       {descriptionList.map((description) => {
-        console.log('descriptio', description);
-        console.log(description.showList);
+        const {
+          eventId,
+          posterLink,
+          ageLimit,
+          genres,
+          rentalDateStart,
+          annotation,
+          trailerLink,
+          showList
+        } = description;
+
         return (
           <>
-            <div className={styles.wrapper} key={description.eventId}>
-              <img className={styles.img} src={description.posterLink} alt="poster" />
+            <div className={styles.wrapper} key={eventId}>
+              <img className={styles.img} src={posterLink} alt="poster" />
 
               <div className={styles.textWrapper}>
                 <p className={styles.title}>
                   {description.name}
-                  <span className={styles.ageLimit}>{description.ageLimit.acronym}</span>
+                  <span className={styles.ageLimit}>{ageLimit.acronym}</span>
                 </p>
 
                 <p className={styles.genres}>
                   <span className={styles.genre}>Жанр:</span>
-                  {description.genres ? (
-                    description.genres.map((genre, index) => (
+                  {genres &&
+                    genres.map((genre, index) => (
                       <span className={styles.genre} key={genre.id}>
                         {genre.name}
 
-                        {index + 1 === description.genres.length ? <span>.</span> : <span>,</span>}
+                        {index + 1 === genres.length ? <span>.</span> : <span>,</span>}
                       </span>
-                    ))
-                  ) : (
-                    <p />
-                  )}
+                    ))}
                 </p>
 
                 <p className={styles.genres}>
                   <span className={styles.genre}>Год:</span>
 
-                  <span className={styles.genre}>
-                    {new Date(description.rentalDateStart).getFullYear()}
-                  </span>
+                  <span className={styles.genre}>{new Date(rentalDateStart).getFullYear()}</span>
                 </p>
 
-                <p className={styles.text}>{description.annotation}</p>
+                <p className={styles.text}>{annotation}</p>
 
-                {description.trailerLink ? (
+                {trailerLink && (
                   <a
                     className={styles.trailerLink}
-                    href={description.trailerLink}
+                    href={trailerLink}
                     target="_blank"
                     rel="noreferrer">
-                    Посмотреть триллер
+                    Посмотреть трейлер
                   </a>
-                ) : (
-                  <div className={styles.trailerLinkNone}>Триллер отсутствует</div>
                 )}
               </div>
             </div>
 
             <div className={styles.wrapperAvailiableDate}>
-              {description.showList ? (
-                Object.keys(description.showList).map((cinema) => {
-                  console.log('cinema', cinema);
+              {showList ? (
+                Object.keys(showList).map((cinema) => {
                   // создать стейт с активной датой, рендер при активной дате, передать inline func,
                   return (
                     <div
                       key={cinema}
                       className={styles.availiableDate}
-                      onClick={() => renderAvaliableSessions(description.showList[cinema])}>
+                      onClick={() => renderAvaliableSessions(showList[cinema])}>
                       {new Date(cinema).getDate() + ' ' + monthNames[new Date(cinema).getMonth()]}
                     </div>
                   );
